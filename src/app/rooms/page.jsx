17f6d42445658fe6ca852d27bd2889ab242d2cc9
Prince <chowdharyprince7375@gmail.com>
2024-12-10 16:41:35 +0530

    about us div fix

--- src/app/rooms/page.jsx
+++ src/app/rooms/page.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 function page() {
   return (
     <div className='w-full'>
-        <div>
-            <img className="relative"
+        <div className='relative'>
+            <img 
             src='./assets/rooms-photos/Rooms_page_img.png' alt='Rooms Designed for Your Comfort'/>
-            <h1 className="font-['Roboto_Serif'] text-4xl font-bold text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <div className='absolute'>
+            <h1 className="font-['Roboto_Serif'] text-6xl font-bold text-black absolute  ">
                 Rooms Designed for Your Comfort
             </h1>
+            </div>
         </div>
         
     </div>
