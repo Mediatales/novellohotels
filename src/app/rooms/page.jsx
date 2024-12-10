fb4f1471747906b6fa55d9174519a63b76f3150e
Prince <chowdharyprince7375@gmail.com>
2024-12-10 15:07:45 +0530

    room page

--- src/app/rooms/page.jsx
+++ src/app/rooms/page.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-        <h1>rooms</h1>
+    <div className='w-full'>
+        <div>
+            <img src='./assets/Rooms_page_img.png' alt='Rooms Designed for Your Comfort'/>
+            <h1>Rooms Designed for Your Comfort</h1>
+        </div>
+        
     </div>
   )
 }
