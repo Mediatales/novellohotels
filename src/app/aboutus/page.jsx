210806882098e53e98dfdb7218e425463a5213cb
Prince <chowdharyprince7375@gmail.com>
2024-12-10 12:28:32 +0530

    about us and assets

--- src/app/aboutus/page.jsx
+++ src/app/aboutus/page.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+
 const page = () => {
   return (
-    <div>
-      about
+    <>
+    {/* welcome section */}
+    <div class>
+      <img src="./assets/Welcome_img.png" />
     </div>
+    {/* the essence of use */}
+    <div>
+      <h1>The Essence of Us</h1>
+      <p>Welcome to The Novello by evergreen, you’re home away from home. Nestled in the Queen of hills Mussoorie Dehradun , our hotel offers a perfect blend of modern comfort and warm, personalised service. Whether you're here for business, leisure, or a special occasion, we provide a unique experience that caters to your every need.
+Our beautifully designed rooms and suites, each thoughtfully equipped with state-of-the-art amenities and plush furnishings to ensure a relaxing and enjoyable stay. We strive to make every guest feel like family, offering exceptional hospitality with attention to detail.
+At The Novello by evergreen, we believe in creating memories, not just stays. From our Roof top restaurant to Beautiful View, we offer everything you need for a truly memorable experience. Our dedicated team is committed to delivering outstanding service and ensuring that your visit is as comfortable and enjoyable as possible.
+Whether you’re here to explore the vibrant local culture, relax and unwind, or attend a business meeting, we’re here to make your stay extraordinary.
+Thank you for choosing The Novello by evergreen. 
+We look forward to welcoming you!</p>
+    </div>
+    </>
   )
 }
 
