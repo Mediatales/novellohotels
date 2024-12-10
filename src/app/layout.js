20cc3240ec5e7760547aa55a1c95b5614da9cd9a
sachinupadhyay268 <sachinupadhyay268@gmail.com>
2024-12-10 16:38:37 +0530

    nav z axis

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,12 @@
-import Navbar from "@/components/common/Navbar"; // Adjust the path to your Navbar component
+import Navbar from "@/components/common/Navbar";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar /> {/* Add the Navbar component here */}
-        <main>{children}</main> {/* Main content for each page */}
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
