f70cc74b60faaefc8029ee682a3f8e25e8f071fb
sachinupadhyay268 <sachinupadhyay268@gmail.com>
2025-01-03 12:42:25 +0530

    fixed

--- src/app/rooms/deluxe/page.jsx
+++ src/app/rooms/deluxe/page.jsx
@@ -1,20 +1,20 @@
- import React from 'react'
+//  import React from 'react'
 
-const page = () => {
-  return (
-    <div>
-    <div className='flex ' >
+// const page = () => {
+//   return (
+//     <div>
+//     <div className='flex ' >
 
-    <div className='flex flex-col'>
-    <img src="https://res.cloudinary.com/derv1v0oi/image/upload/v1734289145/deluxeImg_n26z5o.png" alt="" className='rounded-lg' />
-    <p>At The Novello by Hotel Evergreen, we go above and beyond to provide services that make your stay extraordinary. Enjoy our range of facilities including:</p>
-    <h1 text>Amenities</h1>
-    </div>
+//     <div className='flex flex-col'>
+//     <img src="https://res.cloudinary.com/derv1v0oi/image/upload/v1734289145/deluxeImg_n26z5o.png" alt="" className='rounded-lg' />
+//     <p>At The Novello by Hotel Evergreen, we go above and beyond to provide services that make your stay extraordinary. Enjoy our range of facilities including:</p>
+//     <h1 text>Amenities</h1>
+//     </div>
 
-    </div>
+//     </div>
      
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default page
+// export default page
