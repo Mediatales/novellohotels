1e83e99b4c3712eaf02188dd6adbf1f984a7acec
adeeb-123 <adeebsiddiqui77@gmail.com>
2025-01-29 03:29:52 +0530

    udpated

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,11 +1,10 @@
-// export const BASE_URL = "http://localhost:3000";
-// export const BASE_URL = "https://bossalgo.onrender.com";
+const BASE_URL = "https://novello-backend.onrender.com/api/v1";
 
 export const Rooms = {
-    AllRooms_API: process.env.NEXT_PUBLIC_API_BASE_URL + "/rooms/getRoom",
+    AllRooms_API: BASE_URL + "/rooms/getRoom",
 };
 
 export const Gallery = {
-    AllGallery_API: process.env.NEXT_PUBLIC_API_BASE_URL + "/gallery/getAllGallery",
+    AllGallery_API: BASE_URL + "/gallery/getAllGallery",
 };
 
