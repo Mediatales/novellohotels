393fe2165671a52ab23a8f99258fde0e22c269fd
adeeb-123 <adeebsiddiqui77@gmail.com>
2025-02-02 07:42:42 +0530

    fixed

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://novello-backend.onrender.com/api/v1";
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "https://novello-backend.onrender.com/api/v1";
+// const BASE_URL = "http://localhost:3000/api/v1";
 
 export const Rooms = {
     AllRooms_API: BASE_URL + "/rooms/getRoom"
