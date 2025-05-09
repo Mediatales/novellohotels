849b0c0803940565714f1b587750b9eff30c40dd
Media Tales <work.mediatales@gmail.com>
2025-05-09 15:33:16 +0530

    createorder

--- src/app/api/createOrder/route.js
+++ src/app/api/createOrder/route.js
@@ -1,16 +1,16 @@
-import { NextResponse } from "next/server";
-import Razorpay from "razorpay";
+// import { NextResponse } from "next/server";
+// import Razorpay from "razorpay";
 
-const razorpay = new Razorpay({
-    key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
+// const razorpay = new Razorpay({
+//     key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+//     key_secret: process.env.RAZORPAY_KEY_SECRET,
+// });
 
-export async function POST(req) {
-    const { amount } = await req.json();
-    const order = await razorpay.orders.create({
-        amount,
-        currency: "INR",
-    });
-    return NextResponse.json(order);
-} 
+// export async function POST(req) {
+//     const { amount } = await req.json();
+//     const order = await razorpay.orders.create({
+//         amount,
+//         currency: "INR",
+//     });
+//     return NextResponse.json(order);
+// } 
